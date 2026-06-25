d4e0ef02f7ef6b4fa214d85da943975153fb3da2
SeaotterMS <peter910820@gmail.com>
2026-06-26 00:29:09 +0800

    chore: init a Avalonia Mvvm project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace KaraMovieMaker.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+
+        [ObservableProperty]
+        private int _count;
+
+        [RelayCommand]
+        private void IncrementCount()
+        {
+            Count++;
+        }
+    }
+}
